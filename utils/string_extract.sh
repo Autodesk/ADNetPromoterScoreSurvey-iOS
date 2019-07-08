a256a4726c1c13ec053bbaa767662e44136fdc95
Philipp Berner <phl.berner@gmail.com>
2019-07-07 19:21:48 -0700

    Add string extraction script and english strings

---
+++ utils/string_extract.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ibtool --export-strings-file ADNetPromoterScoreSurvey/View/en.lproj/NPSFeedbackQuestionView.strings ADNetPromoterScoreSurvey/View/Base.lproj/NPSFeedbackQuestionView.xib
+ibtool --export-strings-file ADNetPromoterScoreSurvey/View/en.lproj/NPSScoreQuestionView.strings ADNetPromoterScoreSurvey/View/Base.lproj/NPSScoreQuestionView.xib
+ibtool --export-strings-file ADNetPromoterScoreSurvey/View/en.lproj/NPSSurveyView.strings ADNetPromoterScoreSurvey/View/Base.lproj/NPSSurveyView.xib
+ibtool --export-strings-file ADNetPromoterScoreSurvey/View/en.lproj/NPSThankYouView.strings ADNetPromoterScoreSurvey/View/Base.lproj/NPSThankYouView.xib
